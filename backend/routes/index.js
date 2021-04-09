d054a2ee8591253ff87577dfc6a4cab6acbda3d4
muhsinabdil <muhsinabdil@hotmail.com>
2021-04-09 09:48:55 +0300

    question mediatype model

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,6 +7,7 @@ const Response = require("../utils/response");
 const homeRoutes = require("./home_routes");
 const authRoutes = require("./auth_routes");
 const categoryRoutes = require("./category_routes");
+
 //! temiz kod için yapıyoruz
 //! birden çok servis oluşturacağımız için tek bir yerden yönebileceğiz
 router.use(homeRoutes, authRoutes, categoryRoutes);
