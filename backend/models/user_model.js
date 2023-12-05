d21c29bc48349ffdc766257701ca552bbf05f2ce
muhsinabdil <muhsinabdil@hotmail.com>
2023-12-05 11:28:07 +0300

    created user model

--- backend/models/user_model.js
+++ backend/models/user_model.js
@@ -1,8 +1,11 @@
-
-//! model için önce 
+//! model için önce
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+  name: { type: "string", required: true },
+  mail: { type: "string", required: true },
+  password: { type: "string", required: true, minHeight: 6 },
+  avatar: { public_id: { type: "string", required: true } },
+});
 
-
-module.exports =mongoose.model('UserModel', userSchema);
+module.exports = mongoose.model("UserModel", userSchema);
