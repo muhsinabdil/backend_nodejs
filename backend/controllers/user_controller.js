1379c66e9d200ba80b0a912f0ab2076a2a6e3757
muhsinabdil <muhsinabdil@hotmail.com>
2023-12-05 20:04:38 +0300

    created user model

---
+++ backend/controllers/user_controller.js
@@ -0,0 +1,18 @@
+const userModel = require("../models/user_model.js");
+const bcrypt = require("bcryptjs");
+const jwt = require("bcryptjs");
+
+//! işlemleri yazıyoruz
+//!
+const register = async (req, res) => {
+
+    const {}
+
+};
+const login = async (req, res) => {};
+const logout = async (req, res) => {};
+const forgotPassword = async (req, res) => {};
+const resetPassword = async (req, res) => {};
+
+//! bunları dışarı çıkarıyoruz
+module.exports = { register, login, logout, forgotPassword, resetPassword };
--- backend/models/user_model.js
+++ backend/models/user_model.js
@@ -1,11 +1,18 @@
 //! model için önce
 const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-  name: { type: "string", required: true },
-  mail: { type: "string", required: true },
-  password: { type: "string", required: true, minHeight: 6 },
-  avatar: { public_id: { type: "string", required: true } },
-});
-
+//! Schema içine parameter oluşturuyoruz
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    mail: { type: String, required: true },
+    password: { type: String, required: true, minLength: 6 },
+    avatar: {
+      public_id: { type: String, required: true },
+      url: { type: String, required: true },
+    },
+    role: { type: String, default: "user", required: true },
+  },
+  { timestamps: true }
+);
+//!modeli çıkarıyoruz model adı ardından schema
 module.exports = mongoose.model("UserModel", userSchema);
