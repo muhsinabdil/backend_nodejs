828cf859ec1954d0ada5aa5d521664950e9c66d7
muhsinabdil <muhsinabdil@hotmail.com>
2023-12-07 23:33:52 +0300

    şifre yenile ve node mailer kullanıldı

--- backend/controllers/user_controller.js
+++ backend/controllers/user_controller.js
@@ -147,7 +147,11 @@ const forgotPassword = async (req, res) => {
   }
 };
 
-const resetPassword = async (req, res) => {};
+const resetPassword = async (req, res) => {
+
+
+  
+};
 
 //! bunları dışarı çıkarıyoruz,
 module.exports = { register, login, logout, forgotPassword, resetPassword };
